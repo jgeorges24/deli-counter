9721eba3f42ed02a8e8d6bf0a598df3d76b5de2e
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-02-10 19:52:54 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def line(array)
+
+    if array.size > 0
+
+        modified_array = array.collect.with_index(1) {|people, index|  "#{index}. #{person}"}
+           
+        puts "The line is currently: #{modified_array}"
+    else
+        puts "The line is currently empty."
+    end 
+end
+
+
+
+def take_a_number(another_deli, name)
+
+    another_deli << name
+
+    puts "Weclome, #{name}. You are number #{another_deli.size} in line."
+end
